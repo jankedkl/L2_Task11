de55262ecb71855d37cd47f469807689821c01a9
Your Name <youremail@email.com>
2024-06-11 12:50:48 +0200

    Added a FooterComponent.js

---
+++ src/Components/FooterComponent.js
@@ -0,0 +1,12 @@
+//Import react from react
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2024 The Cotton Club. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
